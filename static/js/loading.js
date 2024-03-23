9976ee02ec521605cb84fece1d978d8ef71eac8e
mhk8691 <mohammadhosseinkaboli86@gmail.com>
2024-03-23 15:08:59 +0330

    loading

---
+++ static/js/loading.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    const loading = document.querySelector(".loader");
+    loading.remove();
+  }, 500);
+});
